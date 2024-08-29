11d4f14b74c3582986f209b200544d57be0513a5
lijunjie2232 <2533584225@qq.com>
2024-08-29 10:54:53 +0800

    fix build

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
 from gpu_waiter import DEVICE, NVGPU, Tasker, Waiter, convertFileSize
 
-__all__ = [
-    "Waiter", "Tasker", "DEVICE", "NVGPU", "convertFileSize"
-]
+__all__ = ["Waiter", "Tasker", "DEVICE", "NVGPU", "convertFileSize"]
